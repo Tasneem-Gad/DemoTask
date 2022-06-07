e5dc5f221f58643b7d00ee495ae3cefd1a62abc0
Tasneem-Gad <tgad@sirius-its.com>
2022-06-07 10:21:23 +0200

    login&Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './Layout/home-page/home-page.component';
+import { LoginComponent } from './User/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Home', component:HomePageComponent},
+  {path: 'Login', component:LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Layout/header/header.component';
+import { HomePageComponent } from './Layout/home-page/home-page.component';
+import { LoginComponent } from './User/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomePageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
